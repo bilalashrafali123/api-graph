5f8cc4040a19ca6f2e0856bd1f1da9a1f94580b4
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-05-30 12:36:15 +0500

    updated...

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.scss"
+
+const App = () => {
+  return (
+    <>
+    <div className="api-heading">
+      <h1>Type of <span className="api">APIs</span> <br /> and Their <span className="cases">Use Cases</span></h1>
+    </div>
+    <div className="api-box-div">
+    <button className="api-box"><h2>API</h2></button>
+    </div>
+    </>
+  )
+}
+
+export default App
