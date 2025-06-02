fd4d4b26641dbe536720c0a259e3872a3568929e
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-06-02 12:07:45 +0500

    updated...

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,62 @@
-// import "./App.scss";
+import "./App.scss";
 
-// const App = () => {
-//   return (
-//     <div className="container">
-//       <div className="api-heading">
-//         <h1>
-//           Type of <span className="api">APIs</span> <br /> and Their{" "}
-//           <span className="cases">Use Cases</span>
-//         </h1>
-//       </div>
-//       <div className="api-box-div">
-//         <button className="api-box">
-//           <h2>API</h2>
-//         </button>
-//       </div>
-//       <div className="line-div">
-//         <div className="line"></div>
-//       </div>
-//       <div className="internal">
-//         <div className="line-div">
-//           <div className="line"></div>
-//         </div>
-//       </div>
-//       <div className="horizontal-line-center">
-//         <div className="horizontal-line"></div>
-//       </div>
-//       <h3 className="text-center">Internal API</h3>
-//       <div className="two-vertical-lines">
-//         <div></div>
-//         <div></div>
-//       </div>
-//       <div className="side-labels">
-//         <div className="side-label">
-//           <h3>Partner API</h3>
-//           <div className="line-under-text"></div>
-//           <div className="horizontal-connected-line">
-//             <div className="vertical-lines-container">
-//               <div className="vertical-line"></div>
-//               <div className="vertical-line"></div>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="side-label">
-//           <h3>Open API</h3>
-//           <div className="line-under-text"></div>
-//           <div className="horizontal-connected-line">
-//           </div>
-//           <div className="two-verticals-lines">
-//         <div></div>
-//         </div>
-//         <div className="two-verticals-lines">
-//         <div></div>
-//         </div>
-//         </div>
-//        </div>
-//       </div>
-//   );
-// };
+const App = () => {
+  return (
+        <div className="container">
+    <div className="api-heading">
+        <h1>
+          Type of <span className="api">APIs</span> <br /> and Their{" "}
+          <span className="cases">Use Cases</span>
+        </h1>
+      </div>
 
-// export default App;
+      <div className="api-box-div">
+        <button className="api-box">
+          <h2>API</h2>
+        </button>
+      </div>
 
+      <div className="line-div">
+        <div className="line"></div>
+      </div>
 
-// import "./App.scss";
+      <div className="internal">
+        <div className="line-div">
+          <div className="line"></div>
+        </div>
+      </div>
 
-// const App = () => {
-//   return (
-//     <div className="container">
-//       <div className="api-heading">
-//         <h1>
-//           Type of <span className="api">APIs</span> <br /> and Their{" "}
-//           <span className="cases">Use Cases</span>
-//         </h1>
-//       </div>
+      <div className="horizontal-line-center">
+        <div className="horizontal-line"></div>
+      </div>
 
-//       <div className="api-box-div">
-//         <button className="api-box">
-//           <h2>API</h2>
-//         </button>
-//       </div>
+      <h3 className="text-center">INTERNAL API</h3>
 
-//       <div className="line-div">
-//         <div className="line"></div>
-//       </div>
+      <div className="two-vertical-lines">
+        <div></div>
+        <div></div>
+      </div>
 
-//       <div className="internal">
-//         <div className="line-div">
-//           <div className="line"></div>
-//         </div>
-//       </div>
-//       <div className="horizontal-line-center">
-//         <div className="horizontal-line"></div>
-//       </div>
-//       <h3 className="text-center">Internal API</h3>
-//       <div className="two-vertical-lines">
-//         <div></div>
-//         <div></div>
-//       </div>
-//       <div className="structure-diagram">
-//         <div className="side-labels">
-//           <div className="side-label">
-//             <h3>Partner API</h3>
-//             <div className="line-under-text"></div>
-//             </div>
-//             <div className="side-label">
-//             <h3>Open API</h3>
-//             <div className="line-under-text"></div>
-//           </div>
-//         </div>
-//          <div  className="horizontal-connector">
-//           <div className="vertical-start"></div>
-//           <div className="horizontal-line"></div>
-//           <div className="vertical-end"></div>
-//          </div>
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="side-labels">
+        <div className="side-label">
+          <h3>Partner API</h3>
+          <div className="line-under-text"></div>
+          <div className="horizontal-connected-line">
+            <div className="vertical-lines-container">
+              <div className="vertical-line"></div>
+              <div className="vertical-line"></div>
+            </div>
+          </div>
+        </div>
 
-// export default App;
+        <div className="side-label">
+          <h3>Open API</h3>
+          <div className="line-under-text"></div>
+          <div className="horizontal-connected-line"></div>
+         </div>
+      </div>
+    </div>
+  );
+};
 
-
+export default App;
